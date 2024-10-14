d07f787344d214d8e64c2a216bd38c8c503756e0
marcinJ212 <jankowiak.gost@gmail.com>
2024-10-14 13:05:12 +0200

    assa

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const helmet = require('helmet');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Użyj PORT z Heroku lub 3000 lokalnie
 
 // Użyj Helmet, aby ustawić nagłówki bezpieczeństwa
 app.use(helmet());
@@ -12,7 +12,7 @@ app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"], // Zezwól na zasoby tylko z tej samej domeny
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Zezwól na skrypty z tej samej domeny oraz na inline (ale należy to robić ostrożnie)
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Zezwól na skrypty z tej samej domeny oraz na inline (należy to robić ostrożnie)
         imgSrc: ["'self'", "data:", "http:", "https:"], // Zezwól na obrazy z tej samej domeny oraz z zewnętrznych
         connectSrc: ["'self'", "https://x8ki-letl-twmt.n7.xano.io"], // Zezwól na połączenia z API Xano
     }
@@ -28,5 +28,5 @@ app.get('/', (req, res) => {
 
 // Uruchomienie serwera
 app.listen(PORT, () => {
-    console.log(`Serwer działa na http://localhost:${PORT}`);
+    console.log(`Serwer działa na porcie ${PORT}`);
 });
