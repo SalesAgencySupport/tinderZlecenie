8e785ee18eb8a8c2a450848d7a97371b0d8e5221
marcinJ212 <jankowiak.gost@gmail.com>
2024-10-14 13:14:17 +0200

    sda

--- server.js
+++ server.js
@@ -8,19 +8,12 @@ const PORT = process.env.PORT || 3000;
 // Użyj Helmet, aby ustawić nagłówki bezpieczeństwa
 app.use(helmet());
 
-// Nagłówek X-Frame-Options: zezwala na osadzanie w iframe
-app.use((req, res, next) => {
-    res.setHeader('X-Frame-Options', 'ALLOW-FROM https://your-bubble-app-url.bubbleapps.io');
-    // lub
-    // res.setHeader('X-Frame-Options', 'ALLOWALL'); // Nie jest zalecane, ale może być użyte do testowania
-    next();
-});
-
-// Ustawienia polityki CSP
+// Zaktualizowane ustawienia polityki CSP
 app.use(helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"], // Zezwól na zasoby tylko z tej samej domeny
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Zezwól na skrypty z tej samej domeny oraz na inline (należy to robić ostrożnie)
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Zezwól na skrypty z tej samej domeny oraz inline (należy to robić ostrożnie)
+        scriptSrcAttr: ["'self'", "'unsafe-inline'"], // Zezwól na używanie atrybutów skryptów
         imgSrc: ["'self'", "data:", "http:", "https:"], // Zezwól na obrazy z tej samej domeny oraz z zewnętrznych
         connectSrc: ["'self'", "https://x8ki-letl-twmt.n7.xano.io"], // Zezwól na połączenia z API Xano
     }
