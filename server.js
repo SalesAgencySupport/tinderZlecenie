97a9e8f7671258b62ad4982c03912df26ff6077b
marcinJ212 <jankowiak.gost@gmail.com>
2024-10-14 13:23:12 +0200

    sda

--- server.js
+++ server.js
@@ -5,18 +5,20 @@ const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Użyj Helmet, ale dostosuj nagłówki CSP
-app.use(helmet());
+// Użyj Helmet, ale dostosuj nagłówki CSP i usuń X-Frame-Options
+app.use(helmet({
+    frameguard: false // Wyłącz nagłówek X-Frame-Options
+}));
 
 // Konfiguracja polityki CSP
 app.use(helmet.contentSecurityPolicy({
     directives: {
         defaultSrc: ["'self'"], // Zezwól na zasoby tylko z tej samej domeny
-        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Zezwól na skrypty z tej samej domeny, inline i eval (należy ostrożnie używać)
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"], // Zezwól na skrypty z tej samej domeny, inline i eval
         scriptSrcAttr: ["'self'", "'unsafe-inline'"], // Zezwól na atrybuty skryptów inline
         imgSrc: ["'self'", "data:", "http:", "https:"], // Zezwól na obrazy z różnych źródeł
         connectSrc: ["'self'", "https://x8ki-letl-twmt.n7.xano.io"], // Zezwól na połączenia z API Xano
-        frameAncestors: ["'self'", "https://your-bubble-domain.com"], // Zezwól na osadzanie w iframe
+        frameAncestors: ["'self'", "https://your-bubble-domain.com"], // Zezwól na osadzanie w iframe z określonej domeny Bubble
     }
 }));
 
